652d542949446bceca4d7d72195df7846d591091
Vijay Pushkin <a.vijaypushkin@gmail.com>
2025-05-26 22:11:29 +0530

    🎉 init(wxt): web extension

---
+++ apps/wxt/src/entrypoints/devtools-panel/main.tsx
@@ -0,0 +1,6 @@
+import { render } from "solid-js/web";
+
+import "./style.css";
+import App from "./App.jsx";
+
+render(() => <App />, document.getElementById("root")!);
---
+++ apps/wxt/src/entrypoints/devtools/main.ts
@@ -0,0 +1,5 @@
+browser.devtools.panels.create(
+  "Gunsole",
+  "icon/128.png",
+  "devtools-panel.html"
+);
